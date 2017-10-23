6ea379888f117fd3ab6522fbaebf2186c1e4f87c
Guest User <Guest@Epicodus-8C.local>
2017-10-23 11:11:40 -0700

    change scripts.js to use forEach loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,13 @@
 $(document).ready(function() {
   $('#formOne').submit(function(event) {
-    var nounInput = $("input#noun").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbingInput = $("input#verbing").val();
-    var personsNameInput = $("input#personsName").val();
-    var adjectiveInput = $("input#adjective").val();
-    var noun2Input = $("input#noun2").val();
-    var personInRoomInput = $("input#personInRoom").val();
-    var numberInput = $("input#number").val();
-    var number2Input = $("input#number2").val();
-    var pluralNounInput = $("input#pluralNoun").val();
-    var noun3Input = $("input#noun3").val();
-    var adjective2Input = $("input#adjective2").val();
-    var colorInput = $("input#color").val();
-    var verbInput = $("input#verb").val();
 
-    $(".noun").text(nounInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verbing").text(verbingInput);
-    $(".personsName").text(personsNameInput);
-    $(".adjective").text(adjectiveInput);
-    $(".noun2").text(noun2Input);
-    $(".personInRoom").text(personInRoomInput);
-    $(".number").text(numberInput);
-    $(".number2").text(number2Input);
-    $(".pluralNoun").text(pluralNounInput);
-    $(".noun3").text(noun3Input);
-    $(".adjective2").text(adjective2Input);
-    $(".color").text(colorInput);
-    $(".verb").text(verbInput);
+    var blanks = ["noun", "exclamation", "verbing", "personsName", "adjective", "noun2", "personInRoom", "number", "number2", "pluralNoun", "noun3", "adjective2", "color", "verb"]
+
+    blanks.forEach(function(blank) {
+      var input = $("input#" + blank).val();
+      $("." + blank).text(input);
+    });
+
 
     $("#story").show();
 
